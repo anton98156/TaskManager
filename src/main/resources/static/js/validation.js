09b12a19b73067bc108315ba11304b067e7ff455
luckyDev <antonkravcov@MacBook-Air-Anton.local>
2024-06-18 12:44:46 +0300

    Updated add button

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -3,10 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     form.addEventListener('submit', function(event) {
         const name = document.getElementById("name").value
-        const description = document.getElementById("description").value
 
-        if (name.trim() === "" || description.trim() === "") {
-            alert("Пожалуйста, заполните все поля!")
+        if (name.trim() === "") {
+            alert("Пожалуйста, введите название задачи!")
             event.preventDefault() // Блокирование формы.
             return false
         }
