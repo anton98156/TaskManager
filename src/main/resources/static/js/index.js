11565b9907787420d08a17b451224f9e5881c363
luckyDev <antonkravcov@MacBook-Air-Anton.local>
2024-06-18 15:28:41 +0300

    Updated index.js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -12,6 +12,29 @@ function redirectToTaskOpenPage(taskId) {
     window.location.href = '/task-open/' + taskId;
 }
 
+function addTaskButtonBehavior() {
+    var taskBodies = document.querySelectorAll('.task-body');
+
+    taskBodies.forEach(function(taskBody) {
+        taskBody.querySelector('.update-button').style.display = 'none';
+        taskBody.querySelector('.delete-button').style.display = 'none';
+    });
+
+    taskBodies.forEach(function(taskBody) {
+        taskBody.addEventListener('mouseover', function() {
+            this.querySelector('.update-button').style.display = 'inline-block';
+            this.querySelector('.delete-button').style.display = 'inline-block';
+        });
+
+        taskBody.addEventListener('mouseout', function() {
+            this.querySelector('.update-button').style.display = 'none';
+            this.querySelector('.delete-button').style.display = 'none';
+        });
+    });
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
+    addTaskButtonBehavior();
     handleTaskBodyClick();
 });
