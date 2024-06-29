04062ad2044e1053acd1d01f8d61fdcd9d9cf5af
luckyDev <antonkravcov@MacBook-Air-Anton.local>
2024-06-29 17:01:50 +0300

    Fields length has been updated

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector('form')
-
+    
     form.addEventListener('submit', function(event) {
         const name = document.getElementById("name").value
 
         if (name.trim() === "") {
             alert("Пожалуйста, введите название задачи!")
+            event.preventDefault() // Блокирование формы.
+            return false
+        }
+
+        if (name.length > 60) {
+            alert("Название задачи слишком длинное!")
             event.preventDefault() // Блокирование формы.
             return false
         }
