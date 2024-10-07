f39157b78b2fc1775ff6dbec856ad78376234064
luckyDev <antonkravcov@MacBook-Air-Anton.local>
2024-10-07 11:41:50 +0300

    Added enum Status to Task

--- src/main/java/com/taskmanager/pro/model/Task.java
+++ src/main/java/com/taskmanager/pro/model/Task.java
@@ -12,6 +12,14 @@ public class Task {
     private boolean urgency;
     private boolean importance;
 
+    // Статусы задачи.
+    public enum Status {
+        ACTIVE,
+        COMPLETED
+    }
+
+    private Status status = Status.ACTIVE; // Значение по умолчанию.
+
     // Кастомизация метода для корректности наименования.
     public boolean getImportance() {
         return importance;
