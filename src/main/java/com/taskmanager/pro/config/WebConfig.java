af3547f08ed3f63f2ba8324ac931356910e2c146
luckyDev <antonkravcov@MacBook-Air-Anton.local>
2024-10-29 18:16:14 +0300

    Added WebConfig

---
+++ src/main/java/com/taskmanager/pro/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.taskmanager.pro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
+}
