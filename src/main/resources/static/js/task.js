f753bf5216698776c58d12ef591b5c26a72ec3a2
luckyDev <antonkravcov@MacBook-Air-Anton.local>
2024-11-05 13:27:08 +0300

    Updated http logic for deleting task

---
+++ src/main/resources/static/js/task.js
@@ -0,0 +1,30 @@
+function handleTaskBodyClick() {
+    var deleteButtons = document.querySelectorAll('#button-delete');
+    deleteButtons.forEach(function(deleteButton) {
+        // Получаем taskId из data-id кнопки
+        var taskId = deleteButton.getAttribute('data-id');
+
+        deleteButton.addEventListener('click', function(event) {
+            event.preventDefault(); // Предотвращает переход по ссылке
+            event.stopPropagation();
+            redirectDeleteTask(taskId);
+        });
+    });
+}
+
+function redirectDeleteTask(taskId) {
+    fetch('/task-delete/' + taskId, {
+        method: 'POST'
+    })
+    .then(response => {
+        if (response.redirected) {
+            window.location.href = response.url;
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Инициализация обработчиков после загрузки страницы
+document.addEventListener('DOMContentLoaded', handleTaskBodyClick);
