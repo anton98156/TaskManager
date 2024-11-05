be74382e858484267da367063e124aba5cd2ad45
luckyDev <antonkravcov@MacBook-Air-Anton.local>
2024-11-05 13:32:49 +0300

    Updated the comments and the code structure

--- src/main/resources/static/js/task.js
+++ src/main/resources/static/js/task.js
@@ -1,11 +1,11 @@
 function handleTaskBodyClick() {
     var deleteButtons = document.querySelectorAll('#button-delete');
     deleteButtons.forEach(function(deleteButton) {
-        // Получаем taskId из data-id кнопки
+        // Получение taskId из data-id кнопки.
         var taskId = deleteButton.getAttribute('data-id');
 
         deleteButton.addEventListener('click', function(event) {
-            event.preventDefault(); // Предотвращает переход по ссылке
+            event.preventDefault(); // Предотвращение перехода по ссылке.
             event.stopPropagation();
             redirectDeleteTask(taskId);
         });
@@ -26,5 +26,5 @@ function redirectDeleteTask(taskId) {
     });
 }
 
-// Инициализация обработчиков после загрузки страницы
+// Инициализация обработчиков после загрузки страницы.
 document.addEventListener('DOMContentLoaded', handleTaskBodyClick);
