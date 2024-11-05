f0346a3b63c466fb4bc87a49c02c722bb870c2a3
luckyDev <antonkravcov@MacBook-Air-Anton.local>
2024-11-05 10:48:31 +0300

    Finished NotificationBuilderTest

--- src/test/java/com/taskmanager/pro/service/NotificationBuilderTest.java
+++ src/test/java/com/taskmanager/pro/service/NotificationBuilderTest.java
@@ -31,13 +31,15 @@ public class NotificationBuilderTest {
     @BeforeEach
     public void setUp() {
         task = new Task();
+        notification = new Notification();
+
         task.setId(1);
         task.setName("Test Task");
         task.setDescription("Description of test task");
 
-        notification = new Notification();
         notification.setId(1);
         notification.setMessage(String.format("Истек срок исполнения по задаче: '%s'.", task.getName()));
+        notification.setRead(false);
     }
 
         // Вспомогательный метод для повышения читаемости кода.
@@ -72,5 +74,32 @@ public class NotificationBuilderTest {
         assertNotNull(notifications);
         assertEquals(1, notifications.size());
         verify(notificationRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void testFindById() {
+        when(notificationRepository.findById(1)).thenReturn(notification);
+
+        Notification actualNotification = notificationBuilder.findById(1);
+
+        assertNotNull(actualNotification);
+        assertEquals(notification, actualNotification);
+        verify(notificationRepository, times(1)).findById(1);
+    }
+
+    @Test
+    public void testToggleRead() {
+        notification.setRead(true);
+        Notification notificationCopy = new Notification();
+
+        notificationCopy.setId(1);
+        notificationCopy.setMessage(notification.getMessage());
+        notificationCopy.setRead(notification.isRead());
+
+        when(notificationRepository.toggleRead(notification)).thenReturn(notificationCopy);
+
+        Notification actualNotification = notificationBuilder.toggleRead(notification);
+        assertEquals(notification.isRead(), actualNotification.isRead());
+        verify(notificationRepository, times(1)).toggleRead(notification);
     }
 }
